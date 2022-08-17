f9d00ee67718037f436ef4d654b5066989d4694d
amir.hamza <hm_xa@yahoo.com>
2022-08-17 17:09:28 +0500

    adding first code to check the mocha framwork functionality

---
+++ cypress/Integration/Examples/Testing.js
@@ -0,0 +1,12 @@
+
+
+describe("My Testing Suit", function(){
+
+    it("My first Test Case", function(){
+        cy.visit("https://www.google.com/")
+    })
+    
+    it("My Second Test Case", function(){
+        cy.visit("https://www.olx.com.lb/")
+    })
+})
