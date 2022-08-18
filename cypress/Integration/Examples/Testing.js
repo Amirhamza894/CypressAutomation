7d70cbf5ac2c58c5da2ddb5dd94030aab10e44f2
amir.hamza <hm_xa@yahoo.com>
2022-08-18 14:02:27 +0500

    adding POM with code

--- cypress/Integration/Examples/Testing.js
+++ cypress/Integration/Examples/Testing.js
@@ -1,12 +1,12 @@
-
+/// <reference types="Cypress" />
 
 describe("My Testing Suit", function(){
 
-    it("My first Test Case", function(){
-        cy.visit("https://www.google.com/")
-    })
-    
     it("My Second Test Case", function(){
         cy.visit("https://www.olx.com.lb/")
+        cy.get("input[type='search']").type('car for sale')
+        cy.get("._0db6bd2f.a3e390b5").click()
+        cy.wait(5000)
+        cy.get("._7e3920c1").should("have.length",45 )
     })
 })
