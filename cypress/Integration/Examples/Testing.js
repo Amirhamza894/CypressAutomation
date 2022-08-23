54ae8e644cf54b6ec483d651b9b8cd39a1c55a67
amir.hamza <hm_xa@yahoo.com>
2022-08-23 16:01:42 +0500

    adding additional things to cypress

--- cypress/Integration/Examples/Testing.js
+++ cypress/Integration/Examples/Testing.js
@@ -7,6 +7,10 @@ describe("My Testing Suit", function(){
         cy.get("input[type='search']").type('car for sale')
         cy.get("._0db6bd2f.a3e390b5").click()
         cy.wait(5000)
-        cy.get("._7e3920c1").should("have.length",45 )
+        cy.get("._7e3920c1").should("have.length", 45)
+        // cy.get("._7e3920c1:visable").should("have.length", 45) -> this will triggar only visable elements
+        cy.get(".ba608fb8.de8df3a3:nth-child(2)").find(".c46f3bfe").should('have.length', 6)
+        cy.get(".ba608fb8.de8df3a3:nth-child(2)").find(".c46f3bfe").contains('Featured')
+        cy.get('.ba608fb8.de8df3a3:nth-child(2)').find('.c46f3bfe').eq(4).find("._1075545d").click({ force: true })
     })
 })
