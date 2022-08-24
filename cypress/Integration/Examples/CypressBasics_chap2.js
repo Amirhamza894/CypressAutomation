d386bff96062c62dda6af5f4c8c941ac3d4ef8c3
amir.hamza <hm_xa@yahoo.com>
2022-08-24 13:06:06 +0500

    Adding checkboxs with cypress code

---
+++ cypress/Integration/Examples/CypressBasics_chap2.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+
+describe("Cypress Basics", function(){
+
+    // Cypress Basics
+    it("Dropdown in cypress", function(){
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        cy.get('#checkBoxOption1').as('checkPoint')
+        cy.get('@checkPoint').check().should('have.value','option1').and('be.checked')
+        cy.get('@checkPoint').uncheck().should('not.be.checked')
+        cy.get('input[type="checkbox"]').as('allCheckbox')
+        cy.get('@allCheckbox').check(['option1', 'option2']).should('be.checked')
+        
+    })
+})
